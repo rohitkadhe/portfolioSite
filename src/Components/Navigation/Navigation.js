2c1b811e8e82a9f583b693c34a23594e76cf301f
rohitkadhe <rkadhe99@gmail.com>
2018-10-29 17:07:38 -0600

    Fixed scaling issue on smaller displays and cleaned up code

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -3,19 +3,14 @@ import './NavigationStyles.css';
 import resume from '../Resume/Resume'
 const Navigation=()=>{
 	return(
-			<header className="navBar">
-			    <nav className="navBarContainer">
-					 <div className='home'><a href='/portfoliosite'>Home</a></div>
-					 <div className='spaceRight'></div>
-			        <div className="navBarContainerItems">
-						   <ul>
-			                <li><a href='/portfoliosite/projects'>Projects</a></li>
-								 <li><a  target='__blank' href={resume}>Resume</a></li>
-								 <li><a  target='__blank'  href="https://www.linkedin.com/in/rohitkadhe">LinkedIn</a></li>
-			            </ul>
-			        </div>
+			    <nav>
+					   <ul className="navBarItems">
+							 <li className='posLeft'><a className='navItem' href='/portfoliosite'>Home</a></li>
+						    <li><a className='navItem' href='/portfoliosite/projects'>Projects</a></li>
+							 <li><a className='navItem'  target='__blank' href={resume}>Resume</a></li>
+							 <li><a className='navItem'  target='__blank'  href="https://www.linkedin.com/in/rohitkadhe">LinkedIn</a></li>
+		            </ul>
 			    </nav>
-			  </header>
 	);
 }
 
