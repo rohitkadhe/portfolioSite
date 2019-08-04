c210000f994a80127a12d43feb15571baa1fab3d
rohitkadhe <rkadhe99@gmail.com>
2019-08-04 14:20:57 -0600

    Updates

---
+++ precache-manifest.9b15d96b991fe38b35ca005be40fa66d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef89f4a5a6a231640825330dff68060",
+    "url": "/portfoliosite/index.html"
+  },
+  {
+    "revision": "8a0aad9a78356bce69d5",
+    "url": "/portfoliosite/static/css/main.f901b8bf.chunk.css"
+  },
+  {
+    "revision": "dc21bee0c000d8d7a81f",
+    "url": "/portfoliosite/static/js/2.fc6b5fd2.chunk.js"
+  },
+  {
+    "revision": "8a0aad9a78356bce69d5",
+    "url": "/portfoliosite/static/js/main.378f6e50.chunk.js"
+  },
+  {
+    "revision": "f347e0ecd3091c6b95f3",
+    "url": "/portfoliosite/static/js/runtime~main.4f4be5de.js"
+  },
+  {
+    "revision": "f7969afac01ff6117714339738e2bd84",
+    "url": "/portfoliosite/static/media/CalgaryHacks.f7969afa.png"
+  },
+  {
+    "revision": "d1d32a77c78933fcf83c49f316a1a080",
+    "url": "/portfoliosite/static/media/FilesMover.d1d32a77.png"
+  },
+  {
+    "revision": "f7cac3cfd946fde169d2620399962a74",
+    "url": "/portfoliosite/static/media/PoolGame.f7cac3cf.png"
+  },
+  {
+    "revision": "271c9c5585b198123388722f317dcb53",
+    "url": "/portfoliosite/static/media/Rohit-kadhe-resume.271c9c55.pdf"
+  }
+]);
