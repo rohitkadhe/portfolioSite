31a964e8ae94ad00b7eaeae96c72815ea2ed8f87
Rohit Kadhe <rkadhe99@gmail.com>
2019-11-11 16:55:01 -0700

    Updated resume

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import './HomeStyles.css';
-const Home=()=>{
-	return(
-			<div className='home'>
-				<div className='main'>
-					<h1 className='header'>Welcome to my website!</h1>
-					<p className='innerText'>My name is Rohit Kadhe and I am a third
-						year Electrical Engineering Student at the University of Calgary with a Minor in Computer Science.
-					</p>
-					<img alt = ''className='image'src='https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a20bc6324f6ef2969d9a7cae56b8d4d1&auto=format&fit=crop&w=1650&q=80'/>
-				</div>
-				</div>
-
-		);
-}
+import React from "react";
+import "./HomeStyles.css";
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="main">
+        <h1 className="header">Welcome to my website!</h1>
+        <p className="innerText">
+          My name is Rohit Kadhe and I am a Electrical Engineering student at
+          the University of Calgary pursuing a minor in Computer Engineering
+        </p>
+        <img
+          alt=""
+          className="image"
+          src="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a20bc6324f6ef2969d9a7cae56b8d4d1&auto=format&fit=crop&w=1650&q=80"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Home;
