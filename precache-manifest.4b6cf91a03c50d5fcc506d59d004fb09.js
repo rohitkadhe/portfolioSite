f30b14fd7fce5359903fbd1239ad588a12a0b2c6
Rohit Kadhe <rkadhe99@gmail.com>
2019-11-11 16:06:19 -0700

    Updates

---
+++ precache-manifest.4b6cf91a03c50d5fcc506d59d004fb09.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "271c9c5585b198123388722f317dcb53",
+    "url": "/portfoliosite/static/media/Rohit-kadhe-resume.271c9c55.pdf"
+  },
+  {
+    "revision": "f7cac3cfd946fde169d2620399962a74",
+    "url": "/portfoliosite/static/media/PoolGame.f7cac3cf.png"
+  },
+  {
+    "revision": "d1d32a77c78933fcf83c49f316a1a080",
+    "url": "/portfoliosite/static/media/FilesMover.d1d32a77.png"
+  },
+  {
+    "revision": "f7969afac01ff6117714339738e2bd84",
+    "url": "/portfoliosite/static/media/CalgaryHacks.f7969afa.png"
+  },
+  {
+    "revision": "f347e0ecd3091c6b95f3",
+    "url": "/portfoliosite/static/js/runtime~main.4f4be5de.js"
+  },
+  {
+    "revision": "f662d5ce8f2c45e7ffb6",
+    "url": "/portfoliosite/static/js/main.d111a591.chunk.js"
+  },
+  {
+    "revision": "dc21bee0c000d8d7a81f",
+    "url": "/portfoliosite/static/js/2.fc6b5fd2.chunk.js"
+  },
+  {
+    "revision": "f662d5ce8f2c45e7ffb6",
+    "url": "/portfoliosite/static/css/main.9831ff1b.chunk.css"
+  },
+  {
+    "revision": "f66341947a21cd05c0a367fb59562eb5",
+    "url": "/portfoliosite/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfoliosite/precache-manifest.9b15d96b991fe38b35ca005be40fa66d.js"
+  "/portfoliosite/precache-manifest.4b6cf91a03c50d5fcc506d59d004fb09.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfoliosite/index.html"), {
+workbox.routing.registerNavigationRoute("/portfoliosite/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
