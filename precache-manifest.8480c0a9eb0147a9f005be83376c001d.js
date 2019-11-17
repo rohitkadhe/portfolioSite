f6186e0a633f98bc0bf34deb6d29d84bec620ad8
Rohit Kadhe <rkadhe99@gmail.com>
2019-11-16 23:48:12 -0700

    Updates

---
+++ precache-manifest.8480c0a9eb0147a9f005be83376c001d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82e5f31eea13a6df8ad3a16fa8a96336",
+    "url": "/portfoliosite/index.html"
+  },
+  {
+    "revision": "52463290c7eaa8987967",
+    "url": "/portfoliosite/static/css/main.5744bd42.chunk.css"
+  },
+  {
+    "revision": "57624c877cfd11eaa3a9",
+    "url": "/portfoliosite/static/js/2.a49d14d4.chunk.js"
+  },
+  {
+    "revision": "52463290c7eaa8987967",
+    "url": "/portfoliosite/static/js/main.0c2f760b.chunk.js"
+  },
+  {
+    "revision": "210bf8f6ea0593e9945d",
+    "url": "/portfoliosite/static/js/runtime-main.c605a3d4.js"
+  },
+  {
+    "revision": "c9982d368c0193272ed2c9e3d6129fed",
+    "url": "/portfoliosite/static/media/Rohit-kadhe-resume.c9982d36.pdf"
+  },
+  {
+    "revision": "64e57715371f1541196da3a1ed8ed228",
+    "url": "/portfoliosite/static/media/backgroundImagePrimary.64e57715.jpg"
+  }
+]);
