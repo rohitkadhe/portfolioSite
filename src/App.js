be21bfbfe024b5c062803980621ba9884cbe43cc
Rohit Kadhe <rkadhe99@gmail.com>
2019-11-17 00:32:09 -0700

    Removed particles temporarily

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import "./App.css";
 import Home from "./Components/Home/Home";
-import Particles from "react-particles-js";
-import Params from "./particlesjs-config.json";
 
 function App() {
   return (
     <div className="app">
-      <Particles className="particles" params={Params} />
       <Home />
     </div>
   );
