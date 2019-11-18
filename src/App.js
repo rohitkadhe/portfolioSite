7862994fc1933e370f3bc667c8489b42e0a61f2b
Rohit Kadhe <rkadhe99@gmail.com>
2019-11-17 22:55:25 -0700

    Background change

--- src/App.js
+++ src/App.js
@@ -3,20 +3,10 @@ import "./App.css";
 import Home from "./Components/Home/Home";
 import Particles from "react-particles-js";
 import Config from "./particlesjs-config.json";
-import BackgroundImage from "./Images/backgroundImagePrimary.jpg";
 
 function App() {
   return (
-    <div
-      className="App"
-      style={{
-        backgroundImage: `url(${BackgroundImage})`,
-        backgroundRepeat: "no repeat",
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        backgroundAttachment: "fixed"
-      }}
-    >
+    <div className="App">
       <Particles className="particles" params={Config} />
       <div id="content">
         <Home />
