40a23809e2e98aefd49b7917cb21eca12d97f314
Rohit Kadhe <rkadhe99@gmail.com>
2020-07-28 14:10:29 -0600

    Updates

---
+++ precache-manifest.fe418daa37092b2fe8ba6f41bb0e5f66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f68afebca42e0209c36e9b7b75bd285",
+    "url": "/portfoliosite/index.html"
+  },
+  {
+    "revision": "ab097fdda061a61d3a55",
+    "url": "/portfoliosite/static/css/main.8ca3ab26.chunk.css"
+  },
+  {
+    "revision": "f4e3163a5de234bb4a56",
+    "url": "/portfoliosite/static/js/2.ea7180d3.chunk.js"
+  },
+  {
+    "revision": "ab097fdda061a61d3a55",
+    "url": "/portfoliosite/static/js/main.00227bb8.chunk.js"
+  },
+  {
+    "revision": "210bf8f6ea0593e9945d",
+    "url": "/portfoliosite/static/js/runtime-main.c605a3d4.js"
+  },
+  {
+    "revision": "c158c5eed038ace0d302e6d1feee5b93",
+    "url": "/portfoliosite/static/media/Rohit_Resume.c158c5ee.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfoliosite/precache-manifest.f87154ddafc8488497ae24fd725e7e81.js"
+  "/portfoliosite/precache-manifest.fe418daa37092b2fe8ba6f41bb0e5f66.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfoliosite/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
